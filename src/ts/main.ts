85320852155925957f2a826417d1b91256be5df0
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2021-02-07 09:17:02 -0800

    A slider you can control the guess speed with

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,31 @@
 import { BoardRenderer } from "./board-renderer";
 import { BoardState } from "./board-state";
 
+function choose<T>(arr: T[]): T {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function makeRandomMove(board: BoardState): BoardState {
+    while (true) {
+        const pieceIndexes = Object.keys(board.pieces)
+            .map(p => parseInt(p))
+            .filter(p => board.pieces[p].moveable);
+        const randomPiece = choose(pieceIndexes);
+        const moves = [
+            {x: 1, y: 0},
+            {x: -1, y: 0},
+            {x: 0, y: 1},
+            {x: 0, y: -1},
+        ];
+        const randomMove = choose(moves);
+        const newBoard = board.createShifted(randomPiece, randomMove);
+        if (newBoard == null) {
+            continue;
+        }
+        return newBoard;
+    }
+}
+
 function init() {
     let board = BoardState.createFromString(`
 ##  ##
@@ -13,35 +38,35 @@ G LL P
   LL  
 `);
 
-    const container = document.querySelector('.container');
-    if (container == null) {
-        throw 'Give me the container!!';
-    }
+    const container = document.querySelector('.container')!;
 
     const boardRenderer = new BoardRenderer();
     boardRenderer.initialize(board);
     container.append(boardRenderer.rootElem);
 
-    setInterval(() => {
-        while (true) {
-            const pieceIndexes = Object.keys(board.pieces).map(p => parseInt(p));
-            const randomPiece = pieceIndexes[Math.floor(Math.random() * pieceIndexes.length)];
-            const moves = [
-                {x: 1, y: 0},
-                {x: -1, y: 0},
-                {x: 0, y: 1},
-                {x: 0, y: -1},
-            ];
-            const randomMove = moves[Math.floor(Math.random() * moves.length)];
-            const newBoard = board.createShifted(randomPiece, randomMove);
-            if (newBoard == null) {
-                continue;
-            }
-            board = newBoard;
-            boardRenderer.update(board);
-            break;
+    let currentInterval: number | undefined = undefined;
+
+    const slider = document.querySelector<HTMLInputElement>('.speed-slider')!;
+    slider.addEventListener('input', (evt) => {
+        clearInterval(currentInterval);
+        currentInterval = undefined;
+
+        let sliderPos = parseFloat(slider.value);
+        if (sliderPos == 0) {
+            return;
         }
-    }, 20);
+
+        // feels nicer if we square this I think
+        sliderPos = sliderPos * sliderPos;
+
+        // 1 -> 20 ms
+        // 0 -> ∞ ms
+        const period = 20 / sliderPos;
+        currentInterval = window.setInterval(() => {
+            board = makeRandomMove(board);
+            boardRenderer.update(board);
+        }, period);
+    });
 }
 
 window.onload = init;
