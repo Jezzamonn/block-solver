024069c920fbe2fed50496e31e3109191ea7892c
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2021-02-07 08:27:29 -0800

    Randomly pick them!!

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -22,26 +22,26 @@ G LL P
     boardRenderer.initialize(board);
     container.append(boardRenderer.rootElem);
 
-    // setInterval(() => {
-    //     while (true) {
-    //         const pieceIndexes = Object.keys(board.pieces).map(p => parseInt(p));
-    //         const randomPiece = pieceIndexes[Math.floor(Math.random() * pieceIndexes.length)];
-    //         const moves = [
-    //             {x: 1, y: 0},
-    //             {x: -1, y: 0},
-    //             {x: 0, y: 1},
-    //             {x: 0, y: -1},
-    //         ];
-    //         const randomMove = moves[Math.floor(Math.random() * moves.length)];
-    //         const newBoard = board.createShifted(randomPiece, randomMove);
-    //         if (newBoard == null) {
-    //             continue;
-    //         }
-    //         board = newBoard;
-    //         boardRenderer.update(board);
-    //         break;
-    //     }
-    // }, 20);
+    setInterval(() => {
+        while (true) {
+            const pieceIndexes = Object.keys(board.pieces).map(p => parseInt(p));
+            const randomPiece = pieceIndexes[Math.floor(Math.random() * pieceIndexes.length)];
+            const moves = [
+                {x: 1, y: 0},
+                {x: -1, y: 0},
+                {x: 0, y: 1},
+                {x: 0, y: -1},
+            ];
+            const randomMove = moves[Math.floor(Math.random() * moves.length)];
+            const newBoard = board.createShifted(randomPiece, randomMove);
+            if (newBoard == null) {
+                continue;
+            }
+            board = newBoard;
+            boardRenderer.update(board);
+            break;
+        }
+    }, 20);
 }
 
 window.onload = init;
